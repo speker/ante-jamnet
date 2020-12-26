35921044a6aea5d62c142c1f6ee03bb611b43448
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-27 02:41:10 +0300

    update io 26

--- apps/get_state.py
+++ apps/get_state.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import jsonify
 import flask_restful as rest
-from model.sqlite import Database
+from model.sqlite import SqLite
 
 
 class GetState(rest.Resource):
@@ -13,5 +13,5 @@ class GetState(rest.Resource):
             response = jsonify({'data': {'success': False, 'code': 403, 'message': 'bad module name'}})
             response.status_code = 403
             return response
-        module_state = Database().get_state(data['module'])
+        module_state = SqLite().get_state(data['module'])
         return {'data': {'success': 'true', 'payload': {'state': module_state}}}
--- apps/status.py
+++ apps/status.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import flask_restful as rest
-from model.sqlite import Database
+from model.sqlite import SqLite
 
 
 class Status(rest.Resource):
@@ -8,7 +8,7 @@ class Status(rest.Resource):
 
     def __init__(self):
         try:
-            self.modules_states = Database().get_states()
+            self.modules_states = SqLite().get_states()
         except Exception as e:
             print(e)
 
