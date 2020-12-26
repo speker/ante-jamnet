496586986827c6a4750cd4a4dd08bfa8dbe9fafb
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-27 00:46:04 +0300

    update io 22

--- helper/gpio.py
+++ helper/gpio.py
@@ -10,6 +10,6 @@ class Gpio:
     def set_digital(port, output):
         GPIO.setup(port, GPIO.OUT)
         if output == 1:
-            GPIO.output(12, GPIO.HIGH)
+            GPIO.output(port, GPIO.HIGH)
         elif output == 0:
-            GPIO.output(12, GPIO.LOW)
+            GPIO.output(port, GPIO.LOW)
