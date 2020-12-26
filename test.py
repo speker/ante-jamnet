6d0c22e159cc71a758f7d01e3a3ddc629f2c4bcf
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-26 16:22:30 +0300

    update io 7

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import time
 io_0 = IoExpander(0X20)
 io_1 = IoExpander(0X21)
 for i in range(8):
-    io_0.set_digital(i, 1)
-    io_1.set_digital(i, 1)
+    io_0.set_digital(1<<i, 1)
+    io_1.set_digital(1<<i, 1)
     i += 1
     time.sleep(1)
 
