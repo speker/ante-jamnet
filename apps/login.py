c33aab79c59247a16dd7a2fd5e95f3572aac215e
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-28 01:42:57 +0300

    update web

--- apps/login.py
+++ apps/login.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import flask_restful as rest
 from flask import jsonify
+from model.postgre import PostGre
+from datetime import datetime, timedelta
+import jwt
 
 
 class Login(rest.Resource):
@@ -13,4 +16,21 @@ class Login(rest.Resource):
             return response
         username = data['username']
         password = data['password']
-        print(username)
+        user = PostGre().check_user(username, password)
+        if len(user) == 0:
+            response = jsonify({'data': {'success': False, 'code': 401, 'message': 'Kullanıcı Adı veya Şifre Hatalı'}})
+            response.status_code = 401
+            return response
+        else:
+            user_id = user[0][0]
+            JWT_SECRET = '7n?P3gjjJqDCbgZrS4QD#hz+a83pLrD3rJwdtWbx-K#%jkYf%B_2n$ha$*bY9CUp'
+            JWT_ALGORITHM = 'HS256'
+            JWT_EXP_DELTA_SECONDS = 1728000
+            payload = {
+                'user_name': username,
+                'user_id': user_id,
+                'exp': datetime.utcnow() + timedelta(seconds=JWT_EXP_DELTA_SECONDS)
+            }
+            jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM).decode('utf-8')
+            PostGre().set_user_token(user_id, username, jwt_token)
+            return {'data': {'success': True, 'token': jwt_token}}
--- model/postgre.py
+++ model/postgre.py
@@ -1,4 +1,25 @@
+import psycopg2
 
 
 class PostGre:
     db = None
+
+    def __init__(self):
+        if self.db is None:
+            self.db = psycopg2.connect(database="jamnet",
+                                       user="reactor",
+                                       password="ReActor2019!!..",
+                                       host="176.53.10.162",
+                                       port="5432")
+            self.db.autocommit = True
+
+    def check_user(self, username, password):
+        cur = self.db.cursor()
+        cur.execute("SELECT * from users where username=%s and password=%s", (username, password))
+        rows = cur.fetchall()
+        return rows
+
+    def set_user_token(self, user_id, username, token):
+        cur = self.db.cursor()
+        update = cur.execute("UPDATE users SET token=%s where username=%s and user_id=%s", (token,username,user_id ))
+        return update
