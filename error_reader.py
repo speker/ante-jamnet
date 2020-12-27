951ac8cd42130b670b33319128fc4ae2fa9d8ff1
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-27 04:06:34 +0300

    update io 26

--- error_reader.py
+++ error_reader.py
@@ -4,24 +4,25 @@ from helper.gpio import Gpio
 last_states = SqLite().get_states()
 
 module_addr = {
-    1: {'bridge': 'gpio', 'io': 9},
-    2: {'bridge': 'gpio', 'io': 25},
-    3: {'bridge': 'gpio', 'io': 10},
-    4: {'bridge': 'gpio', 'io': 24},
-    5: {'bridge': 'gpio', 'io': 22},
-    6: {'bridge': 'gpio', 'io': 23},
-    7: {'bridge': 'io_0', 'io': 55},
-    8: {'bridge': 'gpio', 'io': 17},
-    9: {'bridge': 'gpio', 'io': 18},
-    10: {'bridge': 'gpio', 'io': 15},
-    11: {'bridge': 'io_0', 'io': 55},
-    12: {'bridge': 'gpio', 'io': 14},
+    1: {'bridge': 'gpio', 'io': 9, 'state': 1},
+    2: {'bridge': 'gpio', 'io': 25, 'state': 1},
+    3: {'bridge': 'gpio', 'io': 10, 'state': 1},
+    4: {'bridge': 'gpio', 'io': 24, 'state': 1},
+    5: {'bridge': 'gpio', 'io': 22, 'state': 1},
+    6: {'bridge': 'gpio', 'io': 23, 'state': 1},
+    7: {'bridge': 'io_0', 'io': 55, 'state': 1},
+    8: {'bridge': 'gpio', 'io': 17, 'state': 1},
+    9: {'bridge': 'gpio', 'io': 18, 'state': 1},
+    10: {'bridge': 'gpio', 'io': 15, 'state': 1},
+    11: {'bridge': 'io_0', 'io': 55, 'state': 1},
+    12: {'bridge': 'gpio', 'io': 14, 'state': 1},
 
 }
 for key in module_addr:
     bridge = module_addr[key]['bridge']
     io = module_addr[key]['io']
     if bridge == 'gpio':
-        print(str(key) + ' : ' + str(Gpio().get_digital(io)))
+        module_addr[key]['state'] = Gpio().get_digital(io)
     elif bridge == 'io_0':
-        print(str(key) + ' : ' + str(1))
+        module_addr[key]['state'] = 1
+    print(module_addr[key]['state'])
