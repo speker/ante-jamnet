4d51cadf528dcb3f217500a2c7bf8aaf2b2b65a2
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-27 04:36:09 +0300

    update io 26

--- helper/io_expander.py
+++ helper/io_expander.py
@@ -54,7 +54,7 @@ class IoExpander:
 
     def configuration(self):
         bank_0 = IO0_OUTPUT | IO1_OUTPUT | IO2_OUTPUT | IO3_OUTPUT | IO4_OUTPUT | IO5_OUTPUT | IO6_OUTPUT | IO7_OUTPUT
-        bank_1 = IO0_INPUT | IO1_OUTPUT | IO2_OUTPUT | IO3_OUTPUT | IO4_OUTPUT | IO5_OUTPUT | IO6_OUTPUT | IO7_OUTPUT
+        bank_1 = IO0_OUTPUT | IO1_OUTPUT | IO2_OUTPUT | IO3_OUTPUT | IO4_OUTPUT | IO5_OUTPUT | IO6_OUTPUT | IO7_OUTPUT
         self.bus.write_byte_data(self.address, CONFIGURATION_REG_0, bank_0)
         self.bus.write_byte_data(self.address, CONFIGURATION_REG_1, bank_1)
 
