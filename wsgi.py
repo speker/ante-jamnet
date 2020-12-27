30296d69dfefb211dfbb9dbd00a02d4d1daaef86
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-27 21:36:15 +0300

    update io 27

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from api import app
 
+host = '0.0.0.0'
+port = 8000
 if __name__ == "__main__":
-    app.run()
+    app.run(host=host, port=port, debug=True, threaded=True, use_reloader=True)
