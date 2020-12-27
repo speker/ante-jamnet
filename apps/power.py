505be1b026b1b0dc3ace128f3e027488b945a65b
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-27 06:45:37 +0300

    update io 26

--- apps/power.py
+++ apps/power.py
@@ -4,13 +4,12 @@ import time
 import flask_restful as rest
 from flask import jsonify
 from helper.modules import Modules
-from model.sqlite import SqLite
+from threading import Thread
 
 
 class Power(rest.Resource):
 
-    @staticmethod
-    def post():
+    def post(self):
         data = rest.request.get_json(silent=True)
         if data is None or data['power'] is None or data['modules'] is None:
             response = jsonify({'data': {'success': False, 'code': 403, 'message': 'bad module name'}})
@@ -26,10 +25,15 @@ class Power(rest.Resource):
             elif power == 0:
                 for key in module_state:
                     Modules().write_module(key['module'], key['p1'], key['p2'], 0)
-                time.sleep(1)
-                Modules().write_module(0, 0, 0, 0)
+                work = Thread(target=self.wait_closer)
+                work.start()
             return {'data': {'success': 'true'}}
         except Exception as e:
             response = jsonify({'data': {'success': False, 'code': 500, 'message': e}})
             response.status_code = 500
             return response
+
+    @staticmethod
+    def wait_closer():
+        time.sleep(10)
+        Modules().write_module(0, 0, 0, 0)
