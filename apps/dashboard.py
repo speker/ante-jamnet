f359fe6d4cdec7d762ae91da5de16d84eaf5894b
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-29 23:03:26 +0300

    update web

---
+++ apps/dashboard.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import flask_restful as rest
+from flask import jsonify
+import os
+import time
+from threading import Thread
+from subprocess import check_output
+from model.sqlite import SqLite
+
+
+class Dashboard(rest.Resource):
+    module_template = '<div class="col-md-3"><div class="card card-success"><div class="card-header"><h3 class="card-title">Modül : |module_id| - |module_name|</h3><div class="card-tools"><button type="button" class="btn btn-tool" id="clear_alarm_|module_id|"><i class="fas fa-volume-off"></i></button></div></div><div class="card-body" style="display: block;"><span><strong>Power</strong></span><div class="progress mb-3"><div class="progress-bar bg-|module_progress|" role="progressbar" style="width: |module_state|%"></div></div><div class="btn-group btn-group-toggle"  data-toggle="buttons"><label class="btn btn-|module_high|"><input type="radio" name="options" id="module_high_|module_id|" > High</label><label class="btn btn-|module_middle|"><input type="radio" name="options" id="module_middle_|module_id|" > Middle</label><label class="btn btn-|module_low|"><input type="radio" name="options" id="module_low_|module_id|"> Low</label><label class="btn btn-|module_off| "><input type="radio" name="options" id="module_off_|module_id|"> Off</label></div></div></div></div>'
+
+    def get(self):
+        modules = SqLite().get_states()
+        module_data = []
+        for key in modules:
+            module_id = key[0]
+            module_p1 = key[1]
+            module_p2 = key[2]
+            module_error = key[3]
+            module_power = key[4]
+            module_name = key[5]
+            module_clear = key[6]
+            module_is_active = key[7]
+            if module_is_active == 1:
+                temp = self.module_template
+                temp = temp.replace("|module_id|", str(module_id))
+                temp = temp.replace("|module_name|", module_name)
+                module_data.append(temp)
+
+        return {'data': {'success': True,'modules': ''.join(module_data)}}
