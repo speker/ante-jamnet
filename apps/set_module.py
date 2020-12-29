abcacc76995c1db689db216032fa88b5821537ca
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-30 02:13:20 +0300

    update web

--- apps/set_module.py
+++ apps/set_module.py
@@ -3,12 +3,13 @@ import flask_restful as rest
 from flask import jsonify
 from helper.modules import Modules
 from model.sqlite import SqLite
+import time
+from threading import Thread
 
 
 class SetModule(rest.Resource):
 
-    @staticmethod
-    def post():
+    def post(self):
         data = rest.request.get_json(silent=True)
         if data is None or data['module'] is None:
             response = jsonify({'data': {'success': False, 'code': 403, 'message': 'bad module name'}})
@@ -29,8 +30,26 @@ class SetModule(rest.Resource):
             Modules().write_module(module_addr, p1, p2, power)
             if power == 1:
                 Modules().write_module(0, 0, 0, 1)
+            else:
+                all_module = SqLite().get_states()
+                all_calc = 0
+                for module in all_module:
+                    if module[7] == 1:
+                        all_calc += module[4]
+
+                if all_calc == 0:
+                    work = Thread(target=self.wait_closer)
+                    work.start()
             return {'data': {'success': True}}
         except Exception as e:
             response = jsonify({'data': {'success': False, 'code': 500, 'message': e}})
             response.status_code = 500
             return response
+
+    @staticmethod
+    def wait_closer():
+        SqLite().set_state(0, 0, 0, 0)
+        time.sleep(60)
+        power_state = SqLite().get_state(0)[4]
+        if power_state == 0:
+            Modules().write_module(0, 0, 0, 0)
