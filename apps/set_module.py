4bd4ff694b6154079455d22ec3c2cda1ca2975cb
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-30 02:18:07 +0300

    update web

--- apps/set_module.py
+++ apps/set_module.py
@@ -4,12 +4,12 @@ from flask import jsonify
 from helper.modules import Modules
 from model.sqlite import SqLite
 import time
-from threading import Thread
 
 
 class SetModule(rest.Resource):
 
-    def post(self):
+    @staticmethod
+    def post():
         data = rest.request.get_json(silent=True)
         if data is None or data['module'] is None:
             response = jsonify({'data': {'success': False, 'code': 403, 'message': 'bad module name'}})
@@ -23,10 +23,8 @@ class SetModule(rest.Resource):
             if module_addr == 0:
                 all_module = SqLite().get_states()
                 for module in all_module:
-                    if module[7] == 1 and module[0] != 0:
+                    if module[7] == 1:
                         Modules().write_module(module[0], module[1], module[2], power)
-                work = Thread(target=self.wait_closer)
-                work.start()
                 return {'data': {'success': True}}
 
             Modules().write_module(module_addr, p1, p2, power)
@@ -39,9 +37,8 @@ class SetModule(rest.Resource):
                     if module[7] == 1:
                         all_calc += module[4]
 
-                if all_calc == 0:
-                    work = Thread(target=self.wait_closer)
-                    work.start()
+                if all_calc==0:
+                    Modules().write_module(0, 0, 0, 0)
             return {'data': {'success': True}}
         except Exception as e:
             response = jsonify({'data': {'success': False, 'code': 500, 'message': e}})
