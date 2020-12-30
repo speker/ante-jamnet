db0f9e867ac292e63b92ece21fbd6ea88f8f1a8f
serkan.peker <serkan.peker@prodabilisim.com>
2020-12-30 11:35:42 +0300

    update web

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-
-
-</tr>
+fin = open("/etc/dhcpcd.conf", "rt")
+#output file to write the result to
+fout = open("/etc/dhcpcd.conf", "wt")
+#for each line in the input file
+for line in fin:
+    #read replace the string and write to output file
+    fout.write(line.replace('# fallback to static profile on eth', '# fallback to static profile on ethrrrr'))
+#close input and output files
+fin.close()
+fout.close()
